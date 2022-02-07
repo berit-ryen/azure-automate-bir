295154be29c5ccaee2434a2994fa47611dcdd6de
berit-ryen <bilgrei@online.no>
2022-02-07 14:10:55 +0100

    Oppgave3_test kortversjon

---
+++ src/oppgave3_test.ps1
@@ -0,0 +1,12 @@
+$url= "http://nav-deckofcards.herokuapp.com/shuffle"
+
+$response = Invoke-WebRequest -Uri $Url
+
+$cards = $response.Content | ConvertFrom-Json
+
+$kortstokk = @()
+foreach ($card in $cards) {
+	$kortstokk = $kortstokk + ($card.suit[0] + $card.value + ",")
+}
+
+Write-host "Kortstokk: $kortstokk"
